8df2801c9f35f40b65852af1fdf79c2c452b2024
Hans <lgs840522@gmail.com>
2021-04-15 00:53:51 +0800

    feat(ch02-14): function invocation

---
+++ ch02-14/app.js
@@ -0,0 +1,11 @@
+// 呼叫函數(function invocation)--基礎範例
+
+function b() {
+  console.log('I am b.')
+};
+
+function a() {
+  b()
+};
+
+a();
---
+++ ch02-14/app2.js
@@ -0,0 +1,13 @@
+// 呼叫函數(function invocation)---進階範例
+
+function a() {
+  b()
+  var c
+};
+
+function b() {
+  var d
+};
+
+a();
+var d;

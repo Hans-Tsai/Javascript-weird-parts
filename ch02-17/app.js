50f917ee99c9562d5041fd0b92aa93e3fc976f73
Hans <lgs840522@gmail.com>
2021-04-18 20:05:38 +0800

    feat(ch02-17): blocking scope & ES6: let

---
+++ ch02-17/app.js
@@ -0,0 +1,8 @@
+// 區塊範圍(block scoping) & ES6: let---基礎範例
+
+if (a > b) {
+  let c = true;
+}
+
+// 會印出
+// 錯誤(error)!
---
+++ ch02-17/app2.js
@@ -0,0 +1,13 @@
+// 區塊範圍(block scoping) & ES6: let---進階範例
+
+for (let i =  0; i <= 5; i++) {
+  console.log(i);
+}
+
+// 會印出
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5

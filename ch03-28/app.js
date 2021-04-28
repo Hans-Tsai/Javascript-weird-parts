006e00729d5252bc5d4c6c8918b50e19373f53d9
Hans <lgs840522@gmail.com>
2021-04-29 01:25:56 +0800

    feat(ch03-28): default value

---
+++ ch03-28/app.js
@@ -0,0 +1,9 @@
+// 預設值(default value)
+
+function greet (name) {
+  name = name || 'Hans';   // 在 ES6 推出能設定函數的參數預設值之前，可以這樣寫，來設定參數的預設值
+  console.log('Hello ' + name); 
+}
+
+greet('Tony');   // Hello Tony
+greet();         // Hello Hans

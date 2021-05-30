430b91f303aed5f5bfd80d085dac43283a5f599c
Hans <lgs840522@gmail.com>
2021-05-31 00:03:51 +0800

    feat(ch03-33): JSON & object literal syntax

---
+++ ch03-33/app.js
@@ -0,0 +1,13 @@
+// JSON & 物件實體語法(Object literal syntax) 是不同的東西
+
+var objectLiteral = {
+    firstName: 'Mary',
+    isAProgrammer: true,
+};
+console.log(objectLiteral);   // { firstName: 'Mary', isAProgrammer: true }
+
+var JSONObject = JSON.stringify(objectLiteral);
+console.log(JSONObject);   // '{"firstName":"Mary","isAProgrammer":true}' => 型別為 string
+
+var JSONString = JSON.parse(JSONObject);
+console.log(JSONString);   // { firstName: 'Mary', isAProgrammer: true }  => 型別為 object

52a19ca2a39f188f7b972ef81753e9b2c26441e2
Hans <lgs840522@gmail.com>
2021-05-30 23:22:14 +0800

    feat(ch03-31): object literal syntax

---
+++ ch03-31/app.js
@@ -0,0 +1,18 @@
+// 物件實體語法(Object literal syntax) --- 基礎篇
+
+var person = { 
+  firstName: 'Hans', 
+  lastName: 'Tsai',
+  address: {
+    street: '111 Main St. ',
+    city: 'New York',
+    state: 'NY',
+  },
+};   // 較推薦此寫法，因為易讀性較高
+console.log(person);   // { firstName: 'Hans', lastName: 'Tsai' }
+
+// 也可以用傳統的方法
+var person2 = new Object();   // 正常來說，不建議使用這個方法來建立新物件
+person2.firstName = 'Eric';
+person2.lastName = 'Chen';
+console.log(person2);   // { firstName: 'Eric', lastName: 'Chen' }
---
+++ ch03-31/app2.js
@@ -0,0 +1,21 @@
+// 物件實體語法(Object literal syntax) --- 進階篇
+
+var Hans = { 
+  firstName: 'Hans', 
+  lastName: 'Tsai',
+  address: {
+    street: '111 Main St. ',
+    city: 'New York',
+    state: 'NY',
+  },
+};
+
+function greet (person) {
+  console.log('Hi, ' + person.firstName);  
+}
+
+greet(Hans);   // Hi, Hans
+greet({ 
+  firstName: 'Mary', 
+  lastName: 'Doe',
+});   // Hi, Mary

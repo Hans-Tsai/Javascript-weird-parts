77dbcb391d8862c8816863027c01a0ae8d539327
Hans <lgs840522@gmail.com>
2021-06-01 21:33:00 +0800

    feat(ch03-36): by value & by reference

---
+++ ch03-36/app.js
@@ -0,0 +1,39 @@
+// 傳值(by value) & 傳址(by reference)
+
+/** by value */
+var a = 3;
+var b;
+
+b = a;
+console.log(a);   // 3
+console.log(b);   // 3
+
+a = 2;   // mutate
+console.log(a);   // 2
+console.log(b);   // 3
+
+/** by reference */
+var c = { greeting: 'hi' };
+var d;
+
+d = c;
+console.log(c);   // { greeting: 'hi' }
+console.log(d);   // { greeting: 'hi' }
+
+c.greeting = 'hello';   // mutate
+console.log(c);   // { greeting: 'hello' }
+console.log(d);   // { greeting: 'hello' }
+
+/** another by reference example (even as parameters) */
+function changeGreeting (obj) {
+    obj.greeting = 'Hola';
+}
+
+changeGreeting(d);
+console.log(c);   // { greeting: 'Hola' }
+console.log(d);   // { greeting: 'Hola' }
+
+/** equals operator sets up new memory space (new address) */
+c = { greeting: 'howdy' };
+console.log(c);   // { greeting: 'howdy' }
+console.log(d);   // { greeting: 'Hola' }
